6b6e0507f2655f5b0482a599ee706cad7f262222
mrwiggles <zodiaczx6@gmail.com>
2015-05-31 14:41:38 +0100

    Added end-to-end test to verify main method works correctly

---
+++ src/main/java/com/colinvipurs/application/Start.java
@@ -0,0 +1,8 @@
+package com.colinvipurs.application;
+
+public class Start {
+    public static void main(String[] args) throws Exception {
+        System.in.read();
+        System.out.println("Empty");
+    }
+}
